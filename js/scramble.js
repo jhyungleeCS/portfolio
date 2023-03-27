6c59a36ede83d89ddae4f395fca28659715e45da
Daniel Lee <jhyunglee.cs@gmail.com>
2023-03-27 01:09:37 -0400

    added heading

--- js/scramble.js
+++ js/scramble.js
@@ -1,22 +1,25 @@
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const h2Element = document.querySelector("#div1 h2");
+const h1Element = document.querySelector("#div1 h1");
 
 let iterations = 0;
 
 setInterval(() => {
-  h2Element.innerText = h2Element.innerText.split("").map((letter, index) => {
+  h1Element.innerText = h1Element.innerText.split("").map((letter, index) => {
     if(index < iterations) {
-      return h2Element.dataset.value[index];
+      return h1Element.dataset.value[index];
     }
     return letters[Math.floor(Math.random() * 26)];
   })
   .join(""); 
 
-  if(iterations >= h2Element.dataset.value.length) {
-    iterations = 0;
+  if(iterations >= h1Element.dataset.value.length) {
+    setTimeout(() => {
+      iterations = 0;
+    }, 3000); 
   } else {
     iterations += 1/3;
   }
 }, 30);
+
 
 
